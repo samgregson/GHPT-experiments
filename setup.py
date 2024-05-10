ae13c2095bc991a2afa53d40c401793326d33f79
Sam Gregson <Sam.Gregson@mottmac.com>
2024-05-10 12:34:41 +0100

    setup project, extract out prompts

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GHPT-experiments',
+    version='0.1',
+    description='The aim is to "flow engineer" a Large Language Model (LLM) powered program to generate Grasshopper3d scripts based on a user prompt as input.',
+    packages=find_packages(),
+)
