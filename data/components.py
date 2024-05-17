316679e63c53d91a6726e2e662ebfdbe50a7abef
Sam Gregson <Sam.Gregson@mottmac.com>
2024-05-17 16:50:47 +0100

    mega parameter validation!

--- data/components.py
+++ data/components.py
@@ -1,11 +1,31 @@
 import json
 import os
+from pydantic import BaseModel
+from typing import List
+
+
+class InputOutput(BaseModel):
+    Name: str
+    DataType: str
+    IsOptional: bool
+
+
+class ValidComponent(BaseModel):
+    Name: str
+    Description: str
+    IsCoreComponent: bool
+    Inputs: List[InputOutput]
+    Outputs: List[InputOutput]
+
+
+class ValidComponents(BaseModel):
+    Components: List[ValidComponent]
 
 
 # Load the components.json file to a dictionary variable
-def load_components():
+def load_components() -> ValidComponents:
     # file path needed to work with pytest
     dir_path = os.path.dirname(os.path.realpath(__file__))
     with open(os.path.join(dir_path, 'components.json'), 'r') as f:
         components_dict = json.load(f)
-    return components_dict
+    return ValidComponents.model_validate(components_dict)
--- models/models.py
+++ models/models.py
@@ -1,8 +1,55 @@
-from pydantic import BaseModel, Field, field_validator
+from pydantic import BaseModel, Field, field_validator, model_validator
 from typing import List, Literal, Optional, Union
-from data.components import load_components
+from data.components import ValidComponents, load_components, ValidComponent
 
-components_dict = load_components()
+valid_components = load_components()
+
+
+def find_valid_component_by_name(
+    valid_components: ValidComponents,
+    name: str
+):
+    """
+    Find the valid component with the given name from the list of valid
+    components.
+    Raises a ValueError if the component is not found.
+    """
+    valid_component = next(
+        (comp for comp in valid_components.Components if comp.Name == name),
+        None
+    )
+    if valid_component is None:
+        # this should not happen as the component name is validated
+        # before this validation
+        raise ValueError(f"Component with name {name} not found")
+    return valid_component
+
+
+def find_valid_parameter_by_name(
+    valid_component: ValidComponent,
+    parameter_name: str,
+    input_output_type: Literal["input", "output"]
+):
+    """
+    Find the valid input/output with the given parameter name from the list of
+    valid inputs/outputs of a component.
+    Raises a ValueError if the input/output is not found.
+    """
+    if input_output_type == "Inputs":
+        valid_input_outputs = valid_component.Inputs
+    else:
+        valid_input_outputs = valid_component.Outputs
+    valid_parameter = next(
+        (input_output for input_output in valid_input_outputs
+            if input_output.Name == parameter_name),
+        None
+    )
+    if valid_parameter is None:
+        raise ValueError(f"Component '{valid_component.Name}' does not have an"
+                         f" {input_output_type} parameter called"
+                         f" {parameter_name}. Please choose a valid parameter"
+                         f" from this list: {valid_input_outputs}.")
+    return valid_parameter
 
 
 class NumberSlider(BaseModel):
@@ -66,7 +113,8 @@ class Component(BaseModel):
     @field_validator("Name")
     @classmethod
     def validate_name_exists(cls, v):
-        name_iterator = (component["Name"] for component in components_dict)
+        name_iterator = (component.Name for component in
+                         valid_components.Components)
         if v not in name_iterator:
             raise ValueError(f"The component {v} could not be found, either "
                              "there is a typo or it does not exist. Please "
@@ -138,6 +186,74 @@ class GrasshopperScriptModel(BaseModel):
         description="A piece of advice or instruction related to using the "
                     "grasshopper script"
     )
+
+    @model_validator(mode='after')
+    def validate_parameter_names(self):
+        """
+        Validate that the parameter names in the connections are valid
+        """
+        for connection in self.Connections:
+            component_name_to = self.get_connection_component_name(
+                connection.To
+            )
+            component_name_from = self.get_connection_component_name(
+                connection.From
+            )
+
+            # map to list of valid components
+            valid_component_to = find_valid_component_by_name(
+                valid_components,
+                component_name_to
+            )
+            valid_component_from = find_valid_component_by_name(
+                valid_components,
+                component_name_from
+            )
+
+            # extract parameter from valid component data
+            valid_parameter_to = find_valid_parameter_by_name(
+                valid_component=valid_component_to,
+                parameter_name=connection.To.ParameterName,
+                input_output_type='input'
+            )
+            valid_parameter_from = find_valid_parameter_by_name(
+                valid_component=valid_component_from,
+                parameter_name=connection.From.ParameterName,
+                input_output_type='output'
+            )
+
+            input_parameter_type = valid_parameter_to.DataType
+            output_parameter_type = valid_parameter_from.DataType
+
+            if input_parameter_type != output_parameter_type:
+                raise ValueError(
+                    f"Parameter types do not match for the connection between "
+                    f"{component_name_from} and {component_name_to}. The input"
+                    f"parameter {connection.To.ParameterName} of "
+                    f"{component_name_to} expects a {input_parameter_type} "
+                    f"type, but the output parameter "
+                    f"{connection.From.ParameterName} of {component_name_from}"
+                    f"of {component_name_from} is of type"
+                    f"{output_parameter_type}. Please make sure the types "
+                    f"match.")
+
+        return self
+
+    def get_connection_component_name(
+            self,
+            connection_detail: Union[InputConnectionDetail,
+                                     OutputConnectionDetail]
+    ):
+        id = connection_detail.Id
+
+        script_component = next(
+            (comp for comp in self.Additions if comp.Id == id),
+            None
+        )
+        if script_component is None:
+            raise ValueError(f"Component with id {id} not found")
+
+        return script_component.Name
 
 
 class Components(BaseModel):
