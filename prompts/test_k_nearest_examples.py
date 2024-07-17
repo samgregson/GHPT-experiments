1582dd30c2d8a06fc413851137c98e09118be517
samgregson <samgregson@hotmail.com>
2024-07-17 15:34:38 +0100

    try fix example retreival pytest

--- prompts/test_k_nearest_examples.py
+++ prompts/test_k_nearest_examples.py
@@ -1,13 +1,15 @@
+from typing import List
+from data.examples import Example
 from pipeline.pipeline import pipe_get_examples
 
 
 def test_example_nearest_k_is_ordered():
     input = "create a cup"
-    examples = pipe_get_examples(input)
-    assert input == examples[0]
+    examples: List[Example] = pipe_get_examples(input)
+    assert input == examples[0].Description
 
 
 def test_example_embedding_is_None():
     input = "create a cup"
-    examples = pipe_get_examples(input)
+    examples: List[Example] = pipe_get_examples(input)
     assert examples[0].Embedding is None
