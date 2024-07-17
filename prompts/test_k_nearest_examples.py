1709a37eed5345da30dc0effe1efb5bdcf9abbcd
samgregson <samgregson@hotmail.com>
2024-07-17 15:38:58 +0100

    update example retrieval test input to "Creates a cup shape in grasshopper"

--- prompts/test_k_nearest_examples.py
+++ prompts/test_k_nearest_examples.py
@@ -4,12 +4,12 @@ from pipeline.pipeline import pipe_get_examples
 
 
 def test_example_nearest_k_is_ordered():
-    input = "create a cup"
+    input = "Creates a cup shape in grasshopper"
     examples: List[Example] = pipe_get_examples(input)
     assert input == examples[0].Description
 
 
 def test_example_embedding_is_None():
-    input = "create a cup"
+    input = "Creates a cup shape in grasshopper"
     examples: List[Example] = pipe_get_examples(input)
     assert examples[0].Embedding is None
